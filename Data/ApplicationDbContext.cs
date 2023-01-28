7cb1dd55f97342be27a07161c0c3d521425bd827
ammar <ammar@DESKTOP-70H540S>
2023-01-29 01:08:39 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CRISPR.Models;
+
+namespace CRISPR.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CRISPR.Models.Comment> Comment { get; set; }
+        public DbSet<CRISPR.Models.DataSet> DataSet { get; set; }
+        public DbSet<CRISPR.Models.GeneCode> GeneCode { get; set; }
+        public DbSet<CRISPR.Models.Prop> Prop { get; set; }
+    }
+}
